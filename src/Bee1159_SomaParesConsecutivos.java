c25771b2f293af7e5ea8fd1090e75bcfa28fb6e6
José Wagner Dutra Porto <jwagnerdporto@gmail.com>
2023-08-14 21:26:19 -0300

    Exercicio 1159

--- src/Bee1159_SomaParesConsecutivos.java
+++ src/Bee1159_SomaParesConsecutivos.java
@@ -1,8 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Bee1159_SomaParesConsecutivos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int X, soma = 0;
+		
+		X = sc.nextInt();
+		
+		while (X != 0) {
+			
+			
+			if (X % 2 != 0) {
+				X = X + 1;
+			}
+			
+		
+			soma = X + X + 2 + X + 4 + X + 6 + X + 8; 
+			System.out.println(soma);
+			
+			X = sc.nextInt();
+		}
+		
+	
+		
+		sc.close();
 
 	}
 
